833153fa2056fe8b21b8a2b695c6807f072f3125
rlin07 <rlin01@berkeley.edu>
2023-01-24 20:50:38 -0800

    testing method 1

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -17,11 +17,18 @@ public class ListExercises {
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        return null;
+        for (int i=1; i<=L;i++){
+        if (i%2==0){
+            return i;
+        }else{
+            return null;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
+        if L1.retainAll(L2).size()<L1.size() {
+            return L1.retainAll(L2);
+        }else{
         return null;
     }
 
